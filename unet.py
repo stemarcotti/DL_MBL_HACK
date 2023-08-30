9fcac478e22c929e06b0fee399f67fd7f531b69f
Rachel Banks <rachel.banks@czbiohub.org>
2023-08-30 20:02:37 +0000

    added gunpowder training

--- unet.py
+++ unet.py
@@ -43,7 +43,7 @@ intensity_augment = gp.IntensityAugment(
   shift_max=0.2)
 noise_augment = gp.NoiseAugment(raw)
 
-# stack = gp.Stack(5)
+
 
 request = gp.BatchRequest()
 request[raw] = gp.Roi((0, 0), (150, 150))
@@ -61,4 +61,98 @@ with gp.build(pipeline):
 fig,ax = plt.subplots(1,5)
 for i in range(5):
   ax[i].imshow(batch[gt].data[i,32])
+# %%
+def model_step(model, loss_fn, optimizer, feature, label, activation, prediction_type=None, train_step=True):
+    
+    # zero gradients if training
+    if train_step:
+        optimizer.zero_grad()
+    
+    # forward - pass data through model to get logits
+    logits = model(feature)
+    
+    if prediction_type == "three_class":
+        label=torch.squeeze(label,1)
+        
+    # pass logits through final activation to get predictions
+    predicted = activation(logits)
+
+    # pass predictions through loss, compare to ground truth
+    loss_value = loss_fn(input=predicted, target=label)
+    
+    # if training mode, backprop and optimizer step
+    if train_step:
+        loss_value.backward()
+        optimizer.step()
+
+    # return outputs and loss
+    outputs = {
+        'pred': predicted,
+        'logits': logits,
+    }
+    
+    return loss_value, outputs
+#%%
+in_channels = 1
+num_fmaps = 12
+fmap_inc_factors = 2
+downsample_factors = [[2,2],[2,2]]
+padding = 'same'
+
+out_channels = 3
+activation = torch.nn.Softmax(dim=1)
+loss_fn = torch.nn.CrossEntropyLoss()
+dtype = torch.LongTensor
+final_kernel_size = 1
+
+unet = UNet(in_channels=1,
+           num_fmaps=6,
+           fmap_inc_factor=2,
+           downsample_factors=downsample_factors,
+           padding='same'
+          )
+
+final_conv = torch.nn.Conv2d(
+    in_channels=num_fmaps,
+    out_channels=out_channels,
+    kernel_size=final_kernel_size)
+
+net = torch.nn.Sequential(unet, final_conv, activation)
+optimizer = torch.optim.Adam(net.parameters())
+# %%
+from torchsummary import summary
+summary(net)
+# %%
+# create new array key for the network output
+prediction = gp.ArrayKey('PREDICTION')
+
+# create a train node using our model, loss, and optimizer
+train = gp.torch.Train(
+  net,
+  loss_fn,
+  optimizer,
+  inputs = {
+    'input': raw
+  },
+  loss_inputs = {
+    0: prediction,
+    1: gt
+  },
+  outputs = {
+    0: prediction
+  })
+
+stack = gp.Stack(5)
+
+pipeline = (
+  source +
+  normalize +
+  random_location +
+  stack +
+  train)
+
+request[prediction] = gp.Roi((0,0), (150, 150))
+
+with gp.build(pipeline):
+   batch = pipeline.request_batch(request)
 # %%

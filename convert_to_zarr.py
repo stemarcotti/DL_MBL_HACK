5eeadcd918448ddfa2f4ae751550e8d763674da8
Rachel Banks <rachel.banks@czbiohub.org>
2023-08-30 20:38:52 +0000

    create hierarchical zarr

--- convert_to_zarr.py
+++ convert_to_zarr.py
@@ -1,5 +1,4 @@
 #%%
-#from iohub.ngff import open_ome_zarr
 import os
 import numpy as np
 import tifffile as tiff
@@ -10,18 +9,23 @@ from skimage import data
 from skimage import filters
 
 #%%
-img_path = '/mnt/efs/shared_data/hack/data/20230811/raw/'
-mask_path = '/mnt/efs/shared_data/hack/data/20230811/fixed_labels/'
-store_path = '/mnt/efs/shared_data/hack/data/20230811/20230811_raw.zarr'
+img_path = '/mnt/efs/shared_data/hack/data/20230504/raw/'
+mask_path = '/mnt/efs/shared_data/hack/data/20230504/fixed_labels/'
+store_path = '/mnt/efs/shared_data/hack/data/20230504/20230504_raw.zarr'
 
 img_list = os.listdir(img_path)
-gt_string = '_deconvolved_rho_0.0038_gamma_0.013_m2_Manual_Mask.tiff'
+gt_string = '_m2_rho0.001_gamma0.001_Manual_Mask.tiff'
+n_files = len(img_list)
+#%%
+f = zarr.group()
+for i in range(n_files):
+    f.create_group(f'fov{i}/raw')
+    f.create_group(f'fov{i}/gt')
+# %%
+
+crop_size = 640
 
 f = zarr.open(store_path, 'w')
-
-#%%
-n_files = len(img_list)
-crop_size = 640
 for file in range(n_files):
     # read files
     zyx = tiff.imread(img_path+img_list[file])
@@ -29,26 +33,18 @@ for file in range(n_files):
     gt = tiff.imread(mask_path+gt_filename)    
     print(gt_filename)
 
-    # add dimension to get TZYX
     z,y,x = zyx.shape
-    tzyx = np.expand_dims(zyx, axis=0)
-    gt = np.expand_dims(gt, axis=0)
+
     # crop if needed
     if (y > crop_size and x > crop_size):
-        tzyx = tzyx[:, 0:z, int(y/2 - crop_size//2) : int(y/2 + crop_size//2), 
+        zyx = zyx[0:z, int(y/2 - crop_size//2) : int(y/2 + crop_size//2), 
                     int(x/2 - crop_size//2) : int(x/2 + crop_size//2)]
     
-    # save to zarr 
-    if file == 0:
-        # f = zarr.open(store_path, 'w')
-        f['raw'] = tzyx
-        f['raw'].attrs['resolution'] = (0.25, 0.075, 0.075) # [um]
-        f['ground_truth'] = gt
-        f['ground_truth'].attrs['resolution'] = (0.25, 0.075, 0.075) # [um]
-    else:
-        f['raw'].append(tzyx, axis=0)
-        f['ground_truth'].append(gt, axis=0)
+    f[f'fov{file}/raw'] = zyx
+    f[f'fov{file}/raw'].attrs['resolution'] = (0.25, 0.075, 0.075) # [um]
+    f[f'fov{file}/gt'] = gt
+    f[f'fov{file}/gt'].attrs['resolution'] = (0.25, 0.075, 0.075) # [um]
 
-
+# %%
 
 # %%

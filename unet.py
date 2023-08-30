b74cab1a586000cf79e0cd7bc637cbe1fb59ce12
Stefania <laste.marcotti@gmail.com>
2023-08-30 19:26:39 +0000

    update pipelne unet.py

--- unet.py
+++ unet.py
@@ -17,6 +17,7 @@ folder_path = '/mnt/efs/shared_data/hack/data/20230811/'
 zarr_file = folder_path+'20230811_raw.zarr'
 
 #%%
+# locate zarr containers
 raw = gp.ArrayKey('RAW')
 gt = gp.ArrayKey('GROUND_TRUTH')
 
@@ -27,10 +28,37 @@ source = gp.ZarrSource(
       gt: 'ground_truth'
     },
     {
-      raw: gp.ArraySpec(interpolatable=True),
-      gt: gp.ArraySpec(interpolatable=False)
+      raw: gp.ArraySpec(interpolatable=True, voxel_size=(1,1)),
+      gt: gp.ArraySpec(interpolatable=False, voxel_size=(1,1))
     })
 # %%
+normalize = gp.Normalize(raw)
+random_location = gp.RandomLocation()
+simple_augment = gp.SimpleAugment()
+intensity_augment = gp.IntensityAugment(
+  raw,
+  scale_min=0.8,
+  scale_max=1.2,
+  shift_min=-0.2,
+  shift_max=0.2)
+noise_augment = gp.NoiseAugment(raw)
 
+# stack = gp.Stack(5)
 
+request = gp.BatchRequest()
+request[raw] = gp.Roi((0, 0), (150, 150))
+request[gt] = gp.Roi((0, 0), (150, 150))
+
+pipeline = (
+  source +
+  normalize +
+  random_location)
+
+with gp.build(pipeline):
+  batch = pipeline.request_batch(request)
+
+# %%
+fig,ax = plt.subplots(1,5)
+for i in range(5):
+  ax[i].imshow(batch[gt].data[i,32])
 # %%

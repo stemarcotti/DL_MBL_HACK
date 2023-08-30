1aedafb31686a4572b78254c3b76681d9160974c
Stefania <laste.marcotti@gmail.com>
2023-08-30 14:28:25 +0000

    update convert_to_zarr

--- convert_to_zarr.py
+++ convert_to_zarr.py
@@ -16,7 +16,8 @@ store_path = '/mnt/efs/shared_data/hack/data/20230811/20230811_raw.zarr'
 
 img_list = os.listdir(img_path)
 gt_string = '_deconvolved_rho_0.0038_gamma_0.013_m2_Manual_Mask.tiff'
-print(img_list)
+
+f = zarr.open(store_path, 'w')
 
 #%%
 n_files = len(img_list)
@@ -38,7 +39,7 @@ for file in range(n_files):
     
     # save to zarr 
     if file == 0:
-        f = zarr.open(store_path, 'w')
+        # f = zarr.open(store_path, 'w')
         f['raw'] = tzyx
         f['raw'].attrs['resolution'] = (1, 1) # check
         f['ground_truth'] = gt
@@ -48,32 +49,3 @@ for file in range(n_files):
         f['gt'].append(gt, axis=0)
 
 
-    break
-
-
-# #%%
-# zyx = tiff.imread('./data/20230811/raw/' + imglist[1])
-# z,y,x = zyx.shape
-# tzyx = np.expand_dims(zyx, axis=0)
-# print(tzyx.shape)
-# if (y > 640 and x > 640):
-#     tzyx = tzyx[:, 0:z, int(y/2 - 320) : int(y/2 + 320), int(x/2 - 320) : int(x/2 + 320)]
-# gtname = imglist[1].replace('.tiff', '_deconvolved_rho_0.0038_gamma_0.013_m2_Manual_Mask.tiff')
-# gt = tiff.imread('./data/20230811/fixed_labels/' + gtname)
-# store_path = './data/20230811_raw.zarr'
-# # with open_ome_zarr(
-# #     store_path, layout="fov", mode="a", channel_names=["GFP"]
-# # ) as dataset:
-# #     dataset["img"] = zyx
-# f = zarr.open('sample_data9.zarr', 'w')
-# f['raw'] = tzyx
-# f['raw'].attrs['resolution'] = (1, 1)
-# f['ground_truth'] = gt
-# f['ground_truth'].attrs['resolution'] = (1, 1)
-
-# img2 = tiff.imread('./data/20230811/raw/' + imglist[2])
-# img2 = np.expand_dims(img2, axis=0)
-# img2 = img2[:, 0:z, int(y/2 - 320) : int(y/2 + 320), int(x/2 - 320) : int(x/2 + 320)]
-# print(img2.shape)
-# f['raw'].append(img2, axis=0)
-# # %%

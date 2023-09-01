a90eb52af491954c044462d3090b1b15b0d2a4ac
Rachel Banks <rachel.banks@czbiohub.org>
2023-09-01 19:07:46 +0000

    loops through multiple zarrs

--- gunpowder_augmentor.py
+++ gunpowder_augmentor.py
@@ -1,3 +1,4 @@
+#%%
 import zarr
 import gunpowder as gp
 import math
@@ -6,11 +7,15 @@ import matplotlib.pyplot as plt
 import torch
 
 
-def prepare_gunpowder_pipeline(load_path, output_shape=(30, 128, 128), device='cuda'): #stack_size=5
+def prepare_gunpowder_pipeline(load_path, 
+                               fov_list,
+                               output_shape=(30, 128, 128), 
+                               device='cuda'): #stack_size=5
  
     # Define keys
     raw = gp.ArrayKey('RAW')
     gt = gp.ArrayKey('GROUND_TRUTH')
+    fg = gp.ArrayKey('FOREGROUND')
 
     ########  Define paramters ########
 
@@ -44,44 +49,69 @@ def prepare_gunpowder_pipeline(load_path, output_shape=(30, 128, 128), device='c
 
     normalize_raw = gp.Normalize(raw)
     
-
-
     ##### Gunpowder pipeline #####
+    sources = tuple([])
+    for i, path in enumerate(load_path):
+      print(path)
+      fovs = fov_list[i]
+      print(f'fovs:{fovs}')
+      source = tuple(gp.ZarrSource(
+      path,
+      {
+          raw: f'fov{fov}/raw',
+          gt: f'fov{fov}/gt',
+          fg: f'fov{fov}/fg_mask'
+      },
+      {
+          raw: gp.ArraySpec(interpolatable=True,
+              voxel_size=(1,1,1)),
+          gt: gp.ArraySpec(interpolatable=False,
+              voxel_size=(1,1,1)),
+          fg: gp.ArraySpec(interpolatable=False,
+                              voxel_size=(1,1,1))
+      }) for fov in fovs)
+      source = source 
+      if i==0:
+          sources = source
+      else:
+          sources = sources + source
 
-    # Load the data
-    source = tuple(gp.ZarrSource(
-    load_path,
-    {
-      raw: f'fov{i}/raw',
-      gt: f'fov{i}/gt'
-    },
-    {
-      raw: gp.ArraySpec(interpolatable=True,
-        voxel_size=(1,1,1)),
-      gt: gp.ArraySpec(interpolatable=False,
-        voxel_size=(1,1,1)),
-    })  + normalize_raw + random_location for i in [1]) 
-
-
+    sources += gp.RandomProvider() #+ normalize_raw + random_location
     # Create the pipeline
-    pipeline = source
-    pipeline += gp.RandomProvider()
+    pipeline = sources
+    pipeline += normalize_raw
+    pipeline += random_location
+    #pipeline += gp.RandomProvider()
     pipeline += simple_augment
     pipeline += elastic_augment
-    pipeline += stack
     pipeline += intensity_augment
     pipeline += noise_augment
+    pipeline += gp.Reject(mask=fg, min_masked=0.1)
     
     # Define a batch request
     request = gp.BatchRequest()
     request[raw] = gp.Roi((0, 0, 0), output_shape)
     request[gt] = gp.Roi((0, 0, 0), output_shape)
+    request[fg] = gp.Roi((0,0,0), output_shape)
 
     return pipeline, request
     
 
+#%%
 
+#%%
+# Define keys
+# raw = gp.ArrayKey('RAW')
+# gt = gp.ArrayKey('GROUND_TRUTH')
+# fg = gp.ArrayKey('FOREGROUND')
 
+# request = gp.BatchRequest()
+# request[raw] = gp.Roi((0, 0, 0), output_shape)
+# request[gt] = gp.Roi((0, 0, 0), output_shape)
+# request[fg] = gp.Roi((0,0,0), output_shape)
+
+# with gp.build(pipeline):
+#     batch = pipeline.request_batch(request)
 # # Build the pipeline and request the batch
 # with gp.build(pipeline):
 #   batch = pipeline.request_batch(request)
@@ -105,3 +135,5 @@ def prepare_gunpowder_pipeline(load_path, output_shape=(30, 128, 128), device='c
    # Print the shape of the 'raw' data
 #print("Raw shape: " + str(result_batch[gp.ArrayKey('RAW')].data.shape))
 #print("Ground truth shape: " + str(result_batch[gp.ArrayKey('GROUND_TRUTH')].data.shape))
+
+# %%

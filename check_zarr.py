378b650cb2605ff186dd3a21d0b901df1fd501d2
Laura Breimann <laurabreimann@gmail.com>
2023-09-02 15:45:55 +0000

    script to check zarr dtype

---
+++ check_zarr.py
@@ -0,0 +1,16 @@
+import zarr
+
+load_path = ['/mnt/efs/shared_data/hack/data/20230811/20230811_raw.zarr',
+             '/mnt/efs/shared_data/hack/data/20230504/20230504_raw.zarr']
+fov_list = [[0,1,2,3], [1,2,3]]
+
+
+for i, path in enumerate(load_path):
+        print(path)
+        fovs = fov_list[i]
+        print(f"fovs:{fovs}")
+        print(i)
+        f= zarr.open(path, "r")
+        for k in fovs:
+                print(k)
+                print(f[f"fov{k}/gt"].info)

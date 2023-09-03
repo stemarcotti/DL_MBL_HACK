7e77ad52fe32d4f3169200ea5c769fb70c17b659
Laura Breimann <laurabreimann@gmail.com>
2023-09-03 21:46:09 +0000

    inital script to predict affinitites

---
+++ predict_affs.py
@@ -0,0 +1,124 @@
+import gunpowder as gp
+import h5py
+import io
+import logging
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import requests
+import torch
+import zarr
+
+from funlib.learn.torch.models import UNet, ConvPass
+from gunpowder.torch import Predict
+
+from affs_model import MtlsdModel, WeightedMSELoss, get_model
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+output_shape = gp.Coordinate((8, 88, 88))
+input_shape = gp.Coordinate((24, 128, 128))
+voxel_size = gp.Coordinate((250, 75, 75))
+input_size = input_shape * voxel_size
+output_size = output_shape * voxel_size
+
+# total roi of image to predict on
+total_roi = gp.Coordinate((64, 640, 640))*voxel_size
+     
+
+stack_size = 16
+
+
+model = get_model()
+loss = WeightedMSELoss()
+
+
+def predict(checkpoint, raw_file, raw_dataset):
+    raw = gp.ArrayKey("RAW")
+    pred_affs = gp.ArrayKey("PRED_AFFS")
+
+    scan_request = gp.BatchRequest()
+
+    scan_request.add(raw, input_size)
+    scan_request.add(pred_affs, output_size)
+
+    context = (input_size - output_size) / 2
+
+    source = gp.ZarrSource(
+        raw_file, {raw: raw_dataset}, {raw: gp.ArraySpec(interpolatable=True)}
+    )
+
+    with gp.build(source):
+        total_input_roi = source.spec[raw].roi
+        total_output_roi = source.spec[raw].roi.grow(-context, -context)
+
+    model = MtlsdModel()
+
+    # set model to eval mode
+    model.eval()
+
+    # add a predict node
+    predict = gp.torch.Predict(
+        model=model,
+        checkpoint=checkpoint,
+        inputs={"input": raw},
+        outputs={0: pred_affs},
+    )
+
+    # this will scan in chunks equal to the input/output sizes of the respective arrays
+    scan = gp.Scan(scan_request)
+
+    pipeline = source
+    pipeline += gp.Normalize(raw)
+
+    # raw shape = h,w
+
+    pipeline += gp.Unsqueeze([raw])
+
+    # raw shape = c,h,w
+
+    pipeline += gp.Stack(1)
+
+    # raw shape = b,c,h,w
+
+    pipeline += predict
+    pipeline += scan
+    pipeline += gp.Squeeze([raw])
+
+    # raw shape = c,h,w
+    # pred_lsds shape = b,c,h,w
+    # pred_affs shape = b,c,h,w
+
+    pipeline += gp.Squeeze([raw, pred_affs])
+
+    # raw shape = h,w
+    # pred_lsds shape = c,h,w
+    # pred_affs shape = c,h,w
+
+    predict_request = gp.BatchRequest()
+
+    # this lets us know to process the full image. we will scan over it until it is done
+    predict_request.add(raw, total_input_roi.get_end())
+    predict_request.add(pred_affs, total_output_roi.get_end())
+
+    with gp.build(pipeline):
+        batch = pipeline.request_batch(predict_request)
+
+    return batch[raw].data, batch[pred_affs].data
+
+
+
+
+snap_dir = "/mnt/efs/shared_data/hack/lsd/aff_exp2/snapshot"
+
+f = zarr.open(os.path.join(snap_dir, "newbatch_1251.zarr"), "r")
+
+checkpoint = f
+raw_file = "/mnt/efs/shared_data/hack/data/20230811/20230811_raw.zarr"
+raw_dataset = "fov3/raw"
+
+raw, pred_affs = predict(checkpoint, raw_file, raw_dataset)
+
